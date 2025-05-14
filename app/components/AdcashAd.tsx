d5d0aa683c2984b8c97d6303f3338816624e8452
rithymeth <rithy1331@gmail.com>
2025-05-14 16:32:16 +0700

    fix: update AdcashAd component to use official aclib.js

--- app/components/AdcashAd.tsx
+++ app/components/AdcashAd.tsx
@@ -1,25 +1,39 @@
 'use client';
 
+import Script from 'next/script';
 import { useEffect } from 'react';
 
 interface AdcashAdProps {
   zoneId: string;
 }
 
+declare global {
+  interface Window {
+    aclib: any;
+  }
+}
+
 export default function AdcashAd({ zoneId }: AdcashAdProps) {
   useEffect(() => {
-    // Create and inject the script
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://www.profitablecreativeformat.com/${zoneId}/invoke.js`;
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
+    // Wait for aclib to be loaded
+    const interval = setInterval(() => {
+      if (window.aclib) {
+        window.aclib.runAutoTag({ zoneId });
+        clearInterval(interval);
+      }
+    }, 100);
 
-    return () => {
-      // Cleanup on unmount
-      document.head.removeChild(script);
-    };
+    return () => clearInterval(interval);
   }, [zoneId]);
 
-  return <div className="adcash-zone" data-zone={zoneId}></div>;
+  return (
+    <>
+      <Script
+        id="aclib"
+        src="https://adcash.com/libs/js/aclib.js"
+        strategy="beforeInteractive"
+      />
+      <div id={`ac-${zoneId}`}></div>
+    </>
+  );
 }
