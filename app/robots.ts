0421a3c9912c8ae768c89b7b1e64c77dab34620e
rithymeth <rithy1331@gmail.com>
2025-05-16 13:34:15 +0700

    fix: add type annotation for seasons find callback

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/*', '/private/*'],
     },
-    sitemap: 'https://1331-movies.com/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL || 'https://1331-movies-kh.com'}/sitemap.xml`,
   };
 }
