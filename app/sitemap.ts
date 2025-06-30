e22493e695248cb78574b2621070104f4cda9221
rithymeth <rithy1331@gmail.com>
2025-06-30 10:51:33 +0700

    Update SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -43,5 +43,42 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     }
   ]
 
-  return staticRoutes
+  const movieRoutes: MetadataRoute.Sitemap = await fetch(`${baseUrl}/api/discover/movie?page=1`).then(res => res.json()).then(data => data.results.map((movie: any) => ({
+    url: `${baseUrl}/movie/${movie.id}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: 'weekly',
+    priority: 0.7
+  }))) as MetadataRoute.Sitemap
+
+  const tvRoutes: MetadataRoute.Sitemap = await fetch(`${baseUrl}/api/discover/tv?page=1`).then(res => res.json()).then(data => data.results.map((tv: any) => ({
+    url: `${baseUrl}/tv-shows/${tv.id}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: 'weekly',
+    priority: 0.7
+  }))) as MetadataRoute.Sitemap
+
+  const animeRoutes: MetadataRoute.Sitemap = await fetch(`${baseUrl}/api/anime`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          Page(page: 1, perPage: 50) {
+            media(type: ANIME) {
+              id
+            }
+          }
+        }
+      `,
+    }),
+  }).then(res => res.json()).then(data => data.data.Page.media.map((anime: any) => ({
+    url: `${baseUrl}/anime/${anime.id}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: 'weekly',
+    priority: 0.7
+  }))) as MetadataRoute.Sitemap
+
+  return [...staticRoutes, ...movieRoutes, ...tvRoutes, ...animeRoutes]
 }
